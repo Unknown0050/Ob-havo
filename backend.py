78902057c0cbb26bf3dd7336082a9cd9693e1b9c
Temur <ravshanovtemur006@gmail.com>
2025-04-02 10:07:08 +0500

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = "a662b6036c566b1811faac6053d0eceb"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather_data = None
+    if request.method == 'POST':
+        city = request.form['city']
+        response = requests.get(BASE_URL, params={'q': city, 'appid': API_KEY, 'units': 'metric'})
+        if response.status_code == 200:
+            weather_data = response.json()
+        else:
+            weather_data = {'error': 'Shahar topilmadi'}
+    return render_template('index.html', weather=weather_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
